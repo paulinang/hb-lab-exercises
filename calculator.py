0cef36f288e351dcc3e4ae9f1f6f2a28c1db91f7
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 16:21:05 -0700

    Reduced frequency of str-to-int conversion

--- calculator.py
+++ calculator.py
@@ -5,30 +5,42 @@ Using our arithmetic.py file from Exercise02, create the
 calculator program yourself in this file.
 """
 
+# import all functions from arithmetic module
 from arithmetic import *
 
 
-# Your code goes here
+# make infinite loop
 while True:
+    # reads user input
     input_string = raw_input("> ")
+    # tokenizes input
     tokens = input_string.split()
+
+    # changes str to int based on number of tokens
+    if len(tokens) == 2:
+        num1 = int(tokens[1])
+    elif len(tokens) == 3:
+        num1 = int(tokens[1])
+        num2 = int(tokens[2])
+
+    # responses to different user inputs
     if tokens[0] == "q":
         break
     elif tokens[0] == "+":
-        print add(int(tokens[1]), int(tokens[2]))
+        print add(num1, num2)
     elif tokens[0] == "-":
-        print subtract(int(tokens[1]), int(tokens[2]))
+        print subtract(num1, num2)
     elif tokens[0] == "*":
-        print multiply(int(tokens[1]), int(tokens[2]))
+        print multiply(num1, num2)
     elif tokens[0] == "/":
-        print divide(int(tokens[1]), int(tokens[2]))
+        print divide(num1, num2)
     elif tokens[0] == "square":
-        print square(int(tokens[1])) 
+        print square(num1) 
     elif tokens[0] == "cube":
-        print cube(int(tokens[1]))
+        print cube(num1)
     elif tokens[0] == "pow":
-        print power(int(tokens[1]), int(tokens[2]))
+        print power(num1, num2)
     elif tokens[0] == "mod":
-        print mod(int(tokens[1]), int(tokens[2]))
+        print mod(num1, num2)
     else:
         print "I don't understand"
