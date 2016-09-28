96101a71da634cbf2426f16add9892715633a7cd
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 12:20:45 -0700

    Changed square and cube functions to use **

--- arithmetic.py
+++ arithmetic.py
@@ -11,10 +11,10 @@ def divide(num1, num2):
     return num1 / num2
 
 def square(num1):
-    return num1 * num1 
+    return num1 ** 2
 
 def cube(num1):
-    return num1 * num1 * num1
+    return num1 ** 3
 
 def power(num1, num2):
     return num1 ** num2
