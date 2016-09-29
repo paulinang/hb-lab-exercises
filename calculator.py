11d3ff25677f28e7aef04bd586e6208661455853
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 17:55:34 -0700

    calculator accounts for any amount of numerical inputs with process_input function

--- calculator.py
+++ calculator.py
@@ -8,39 +8,37 @@ calculator program yourself in this file.
 # import all functions from arithmetic module
 from arithmetic import *
 
+def process_input(input_string):
+    tokens = input_string.split()
+    operator = tokens[0]
+    numlist = map(int, tokens[1:])
+    return operator, numlist
+
 
 # make infinite loop
 while True:
     # reads user input
     input_string = raw_input("> ")
-    # tokenizes input
-    tokens = input_string.split()
-
-    # changes str to int based on number of tokens
-    if len(tokens) == 2:
-        num1 = int(tokens[1])
-    elif len(tokens) == 3:
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
+    operator, numlist = process_input(input_string)
 
     # responses to different user inputs
-    if tokens[0] == "q":
+    if operator == "q":
         break
-    elif tokens[0] == "+":
-        print add(num1, num2)
-    elif tokens[0] == "-":
-        print subtract(num1, num2)
-    elif tokens[0] == "*":
-        print multiply(num1, num2)
-    elif tokens[0] == "/":
-        print divide(num1, num2)
-    elif tokens[0] == "square":
-        print square(num1) 
-    elif tokens[0] == "cube":
-        print cube(num1)
-    elif tokens[0] == "pow":
-        print power(num1, num2)
-    elif tokens[0] == "mod":
-        print mod(num1, num2)
+    elif operator == "+":
+        print add(numlist)
+    elif operator == "-":
+        print subtract(numlist)
+    elif operator == "*":
+        print multiply(numlist)
+    elif operator == "/":
+        print divide(numlist)
+    elif operator == "square":
+        print square(numlist) 
+    elif operator == "cube":
+        print cube(numlist)
+    elif operator == "pow":
+        print power(numlist)
+    elif operator == "mod":
+        print mod(numlist)
     else:
         print "I don't understand"
