54d7efc37cbebbb75472a877cdbdf2911388df3e
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 12:26:44 -0700

    Added with statement and .strip

--- wordcount.py
+++ wordcount.py
@@ -2,17 +2,17 @@
 filename = 'twain.txt'
 
 #open text file to object
-txt_file = open(filename)
+with open(filename) as txt_file:
 
-#initialize empty dictionary
-word_count = {}
+    #initialize empty dictionary
+    word_count = {}
 
-#fill dictionary with word count 
-for line in txt_file:
-    words = line.split()
-    for word in words:
-        word_count[word] = word_count.get(word, 0) + 1
+    #fill dictionary with word count 
+    for line in txt_file:
+        words = line.strip().split()
+        for word in words:
+            word_count[word] = word_count.get(word, 0) + 1
 
-#iterate through dictionary and print each pair
-for word, count in word_count.iteritems():
-    print word, count
+    #iterate through dictionary and print each pair
+    for word, count in word_count.iteritems():
+        print word, count
