d841ba7bf2501348ce8cae3383d31dd6e5ae01a6
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 16:29:55 -0700

    Added user input feature

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,6 +1,6 @@
 def print_ratings(filename):
     """Prints restaurant ratings from given file."""
-    
+
     with open(filename) as text_file:
 
         # create dictionary of restaurant ratings
@@ -8,10 +8,16 @@ def print_ratings(filename):
 
         for line in text_file:
             restaurant, rating = line.strip().split(":")
-            restaurant_scores[restaurant] = rating
+            restaurant_scores[restaurant] = int(rating)
+
+        # get new restaurant and rating from user
+        new_name = raw_input('Enter restaurant name: ')
+        new_rating = int(raw_input('Enter the rating: '))
+        restaurant_scores[new_name] = new_rating
 
         # print sorted list of tuples containing restaurant info
         for restaurant, rating in sorted(restaurant_scores.iteritems()):
             print "{} is rated at {}.".format(restaurant, rating)
 
 print_ratings('scores.txt')
+
